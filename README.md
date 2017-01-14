ca813c4e4c656371558921b0a0a22c031551a5b2
eezing <eric@ezinterweb.com>
2017-01-14 10:56:24 -0800

    converted to non-express

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # redirect-to
 
-A micro service to redirect all GET requests to target URL defined in **process.env.REDIRECT_URL**
+A micro service to redirect all requests to target URL.
 
 The intnended use case is to redirect users to root when landing on the www subdomain.
-
-**Attention:**
-
-- Uses redirect code 301 (Moved Permanently)
-- Only GET requests
 
 ## Get Started
 
 **Run cmd:** ``` $ REDIRECT_URL=https://target-domain.com node index ```
 
 **Result:** localhost:3000/about --> https://target-domain.com/about
+
+## Environment Variables
+
+- REDIRECT_URL: target url to redirect to
+- PORT: port to listen on (default 3000)
+- HTTP_CODE: http status code (default 302)
--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 'uset strict';
 
-const app = require('express')();
+const http = require('http');
+const url = require('url');
+
 const redirectUrl = process.env.REDIRECT_URL;
 const port = process.env.PORT || 3000;
+const httpCode = process.env.HTTP_CODE || 302;
 
-app.get('*', (req, res) => res.redirect(301, `${redirectUrl}${req.path}`));
+if (redirectUrl === undefined) {
+    throw 'process.env.REDIRECT_URL is not defined';
+}
 
-app.listen(port, (err) => err || console.log(`Listening on port ${port}`));
+if (Number(httpCode) < 300 || Number(httpCode) > 308) {
+    throw `process.env.HTTP_CODE (${httpCode}) is not a valid http redirect status code`;
+}
+
+const server = http.createServer((req, res) => {
+
+    const parsedUrl = url.parse(req.url);
+
+    res.writeHead(httpCode, {'Location': `${redirectUrl}${parsedUrl.path}`});
+
+    res.end();
+});
+
+server.listen(port, (err) => {
+
+    if (err) {
+        return;
+    }
+
+    console.log(`listening on port ${port}`); // eslint-disable-line
+});
